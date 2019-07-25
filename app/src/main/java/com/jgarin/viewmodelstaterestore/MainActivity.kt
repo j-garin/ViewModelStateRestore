3915883cb8637079ffbb98d7b69a69db9afc078b
jgarin <garastovych@gmail.com>
2019-07-25 12:35:07 -0700

    Added basic navigation

--- app/src/main/java/com/jgarin/viewmodelstaterestore/MainActivity.kt
+++ app/src/main/java/com/jgarin/viewmodelstaterestore/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.jgarin.viewmodelstaterestore
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.jgarin.viewmodelstaterestore.activity_based.workflow_one.WorkflowOneActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_main)
+
+		btnActivityBased.setOnClickListener {
+			startActivity(Intent(this, WorkflowOneActivity::class.java))
+		}
+	}
 }
--- app/src/main/java/com/jgarin/viewmodelstaterestore/activity_based/workflow_one/WorkflowOneActivity.kt
+++ app/src/main/java/com/jgarin/viewmodelstaterestore/activity_based/workflow_one/WorkflowOneActivity.kt
@@ -1,24 +1,40 @@
 package com.jgarin.viewmodelstaterestore.activity_based.workflow_one
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import com.jgarin.viewmodelstaterestore.R
 import com.jgarin.viewmodelstaterestore.activity_based.base.BaseViewModel
 import com.jgarin.viewmodelstaterestore.activity_based.base.BaseWorkflowActivity
 import com.jgarin.viewmodelstaterestore.activity_based.base.LayoutResId
+import com.jgarin.viewmodelstaterestore.activity_based.workflow_one.screen_01_01.FragmentOneOne
+import com.jgarin.viewmodelstaterestore.activity_based.workflow_one.screen_01_02.FragmentOneTwo
+import com.jgarin.viewmodelstaterestore.activity_based.workflow_one.screen_01_03.FragmentOneThree
 import com.jgarin.viewmodelstaterestore.extensions.viewModelFactory
 
-class WorkflowOneActivity: BaseWorkflowActivity<WorkflowOneState, WorkflowOneScreen, WorkflowOneEvent>() {
+class WorkflowOneActivity : BaseWorkflowActivity<WorkflowOneState, WorkflowOneScreen, WorkflowOneEvent>() {
 
 	override val layout: LayoutResId = R.layout.fragment_container
 
 	override fun getViewModel(savedState: Bundle?): BaseViewModel<WorkflowOneState, WorkflowOneScreen, WorkflowOneEvent> {
 		return ViewModelProviders.of(this, viewModelFactory { WorkflowOneViewModel(savedState) })
-			.get(WorkflowOneViewModel::class.java)
+				.get(WorkflowOneViewModel::class.java)
 	}
 
 	override fun handleScreenChange(screen: WorkflowOneScreen) {
-		TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+		val currentFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainer)
+		when (screen) {
+			WorkflowOneScreen.ScreenOne   -> if (currentFragment !is FragmentOneOne) replaceFragment(FragmentOneOne())
+			WorkflowOneScreen.ScreenTwo   -> if (currentFragment !is FragmentOneTwo) replaceFragment(FragmentOneTwo())
+			WorkflowOneScreen.ScreenThree -> if (currentFragment !is FragmentOneThree) replaceFragment(FragmentOneThree())
+		}
+	}
+
+	private fun replaceFragment(newFragment: Fragment) {
+		supportFragmentManager
+				.beginTransaction()
+				.replace(R.id.fragmentContainer, newFragment)
+				.commit()
 	}
 
 }
